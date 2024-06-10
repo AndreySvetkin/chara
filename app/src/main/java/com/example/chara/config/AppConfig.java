0bd8001d7484dccf62dac5dae460ac8d6a6496d1
ElevenIsNot11 <ily20013@yandex.ru>
2024-06-10 20:14:02 +0400

    ksadkjhdasklhas

--- app/src/main/java/com/example/chara/config/AppConfig.java
+++ app/src/main/java/com/example/chara/config/AppConfig.java
@@ -22,7 +22,7 @@ public class AppConfig {
 
         return new Retrofit.Builder()
                 .addConverterFactory(gsonConverterFactory)
-                .baseUrl("http://192.168.1.5:8080/app/rest/v2/")
+                .baseUrl("http://192.168.0.104:8080/app/rest/v2/")
                 .build();
     }
 }
